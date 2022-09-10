f15aca0b4e7295930ab08145f6b4c7c55f106787
Julia <jdaianova@gmail.com>
2022-09-10 18:53:29 +0200

    fixed eslint

--- src/js/sorthp.js
+++ src/js/sorthp.js
@@ -1,3 +1,3 @@
 export default function sortHP(characters) {
-  return characters.sort((a, b) => a.health > b.health ? -1 : 1);
+  return characters.sort((a, b) => (a.health > b.health ? -1 : 1));
 }
